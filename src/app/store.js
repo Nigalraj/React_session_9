6ba7d21cf736235f1570009b7835842d1564b1d5
Nigalraj Jayasankar <nigalraj.jayasankar@concertidc.com>
2024-07-31 20:20:44 +0530

    first commits

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from '../features/product/apiSlice';
-import productReducer from '../features/product/productSlice';
 
 const store = configureStore({
   reducer: {
-    product: productReducer,
     [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
